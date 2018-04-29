8577231c8bd389e9428f1c1dc093ad9749d31702
Norman Golatka (Lifebook) <norman.golatka@nogosite.de>
2018-04-29 18:22:30 +0200

    initial commit

---
+++ apt-get_upgrade.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# This script updates all packages on the system and removes any
+# packages that are no longer required. It summarizes everything
+# and creates a report file which can e.g. be emailed to someone.
+
+
+seconds=2                                        # Delay time after execution
+output_file="/var/log/apt/apt-get-upgrade.log"   # Report file
+
+echo "apt-get on `hostname` from `date "+%d.%m.%Y, %H:%M"`
+______________________________________________
+
+check
+-----" > $output_file
+
+apt-get check >> $output_file
+sleep $seconds
+
+echo "
+update
+------" >> $output_file
+
+apt-get update -q >> $output_file
+sleep $seconds
+
+echo "
+upgrade
+-------" >> $output_file
+
+apt-get upgrade -y >> $output_file
+sleep $seconds
+
+echo "
+autoremove
+----------" >> $output_file
+
+apt-get autoremove -y >> $output_file
+sleep $seconds
+
+echo "
+______________________________________________
+
+End of report." >> $output_file
