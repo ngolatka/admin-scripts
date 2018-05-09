2b8174ffaa542425b66079fb7f9647c0b40d0e10
Norman Golatka (Lifebook) <norman.golatka@nogosite.de>
2018-05-09 14:48:11 +0200

    clean up apt cache, reorder execution, improve log file readability

--- apt-get_upgrade.sh
+++ apt-get_upgrade.sh
@@ -1,41 +1,45 @@
 #!/bin/bash
 
-# This script updates all packages on the system and removes any
-# packages that are no longer required. It summarizes everything
-# and creates a report file which can e.g. be emailed to someone.
+# This script updates all packages on the system and removes any packages
+# that are no longer required. It summarizes everything and creates a
+# report file which can e.g. be emailed to someone.
 
-# This is intended for use in Cronjobs, therefore no output is
-# shown on the terminal. STDERR from executed commands will also
-# be redirected.
+# The general aim is to keep the system up to date, and automatically
+# remove anything that is no longer needed (e.g. old dependencies) so
+# that nothing grows out of control (apt caches, storage use, etc.).
+
+# This is intended for use in Cronjobs, therefore no output is shown
+# on the terminal. STDERR from executed commands will also be redirected. 
+# The terminal will be completely silent in any case.
 
 
-output_file="/var/log/apt/apt-get_upgrade.log"
+logfile="/var/log/apt/apt-get_upgrade.log"
+
 commands=(
-  "apt-get check"
   "apt-get update -q"
   "apt-get upgrade -q -y"
-  "DEBIAN_FRONTEND=noninteractive"    # Needed before autoremove to supress warning
+  "DEBIAN_FRONTEND=noninteractive"     # Needed before autoremove to supress warnings from debconf
   "apt-get autoremove -y"
+  "apt-get autoclean"
+  "apt-get check"
 )
 
-echo "apt-get on `hostname` on `date "+%d.%m.%Y, %H:%M"`
-" >> $output_file
+echo -e "apt-get on `hostname` on `date "+%d.%m.%Y, %H:%M"`\n" >> $logfile
 
 for command in "${commands[@]}";
 do
 
-  echo -e "$command\n--------" >> $output_file
-
-  eval "$command" >> $output_file 2>&1     # Redirect STDERR to STDOUT, which then goes into the file
+  echo -e "$command\n--------" >> $logfile
+  eval "$command" >> $logfile 2>&1     # Redirect STDERR to STDOUT, which then goes into the logfile
 
   if [ $? -eq 0 ];
   then
 
-    echo -e "OK\n" >> $output_file
-
+    echo -e "OK\n" >> $logfile         # Not all commands say if they executed ok, so we add that here
+  
   else
 
-    echo -e "Aborting\n\nFinished with errors" >> $output_file
+    echo -e "Aborting\n\nFinished with errors\n------------------------\n" >> $logfile
     exit 1
 
   fi
@@ -44,5 +48,5 @@ do
 
 done
 
-echo "Finished, no errors" >> $output_file
+echo -e "Finished, no errors\n------------------------\n" >> $logfile
 exit 0
